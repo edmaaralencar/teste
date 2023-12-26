e34a262e020beca3185ca183e6cb70ffa95ad8a3
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-12-26 16:47:20 -0300

    Adição do arquivo oi.js

--- hello-world.js
+++ hello-world.js
@@ -8,3 +8,11 @@ console.log("Hello World!")
 // git add <nomedoarquivo ou pasta>
  // git add hello-world.js
  // git add . -> adicionar todos arquivos
+
+// git commit -m "Adição de arquivos"
+
+// git branch -M main -> para utilizar 1 vez a depender da necessidade
+
+// git remote add origin https://github.com/edmaaralencar/teste.git (conectar repositório remoto com local) (só roda 1 vez)
+
+// git push -u remote main
