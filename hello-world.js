cc03a9e35a8e200da78eb902e590938f6b6f6b86
Edmar Rocha Alencar <edmaaralencar1@gmail.com>
2023-12-26 16:41:41 -0300

    Arquivos aleatorios

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,10 @@
 console.log("Hello World!")
+
+// Repositório remoto -> Github
+// Repositório local -> seu computador
+
+// git init - para inicializar o repositório (1 vez)
+
+// git add <nomedoarquivo ou pasta>
+ // git add hello-world.js
+ // git add . -> adicionar todos arquivos
